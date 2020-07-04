5fe812ce84807ac93ed4e9f2f088cb71ce22705f
Gergely Reményi <geri@geriremenyi.com>
2020-07-04 10:01:00 +0200

    Wire in epic middleware

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,7 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { createEpicMiddleware } from 'redux-observable';
 
 import { rootReducer } from './RootReducer';
 
+// Reducer
+const reducer = rootReducer;
+
+// Middlewares
+const epicMiddleware = createEpicMiddleware();
+const middleware = [...getDefaultMiddleware(), epicMiddleware]
+
+// Configure store
 export const store = configureStore({
-    reducer: rootReducer
+    reducer,
+    middleware,
+    devTools: process.env.NODE_ENV !== 'production'
 })
