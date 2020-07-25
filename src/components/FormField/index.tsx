b869c1e8459724f2c0c8a0f66ad0fce31644709d
Gergely Reményi <geri@geriremenyi.com>
2020-07-25 10:32:00 +0200

    Adding form field component

--- src/components/FormField/index.tsx
+++ src/components/FormField/index.tsx
@@ -1,3 +1,4 @@
+export { FormField } from './FormField';
 export { FormFieldType } from './FormFieldType';
 export type { IFormFieldProps } from './IFormFieldProps';
 export type { IFormFieldState } from './IFormFieldState';
