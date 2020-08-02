de419cd67b9a16d0b053300a38603724b6a975eb
Gergely Reményi <geri@geriremenyi.com>
2020-08-02 12:33:00 +0200

    Project file updates

--- craco.config.js
+++ craco.config.js
@@ -4,22 +4,28 @@ module.exports = {
     webpack: {
         alias: {
             '~app': path.resolve(__dirname, "./src/app"),
+            '~user': path.resolve(__dirname, "./src/app/user"),
+            '~login': path.resolve(__dirname, "./src/app/user/login"),
+            "~common": path.resolve(__dirname, "./src/common"),
+            "~api": path.resolve(__dirname, "./src/common/api"),
+            "~router": path.resolve(__dirname, "./src/common/router"),
+            "~utilities": path.resolve(__dirname, "./src/common/utilities"),
             "~components": path.resolve(__dirname, "./src/components"),
             "~store": path.resolve(__dirname, "./src/store"),
-            "~utilities": path.resolve(__dirname, "./src/utilities"),
-            "~api": path.resolve(__dirname, "./src/common/api"),
-            "~":  path.resolve(__dirname),
         }
     },
     jest: {
         configure: {
             moduleNameMapper: {
                 "~app/(.*)": "<rootDir>/src/app/$1",
-                "~components/(.*)": "<rootDir>/src/components/$1",
-                "~store/(.*)": "<rootDir>/src/store/$1",
-                "~utilities/(.*)": "<rootDir>/src/utilities/$1",
+                "~user/(.*)": "<rootDir>/src/app/user/$1",
+                "~login/(.*)": "<rootDir>/src/app/user/login/$1",
+                "~common/(.*)": "<rootDir>/src/common/$1",
                 "~api/(.*)": "<rootDir>/src/common/api/$1",
-                "~/(.*)": "<rootDir>/src/$1"
+                "~router/(.*)": "<rootDir>/src/common/router/$1",
+                "~utilities/(.*)": "<rootDir>/src/common/utilities/$1",
+                "~components/(.*)": "<rootDir>/src/components/$1",
+                "~store": "<rootDir>/src/store",
             }
         }
     }
