3b61aead2f852a414cfd4501bc090e7917aa9305
Gergely Reményi <geri@geriremenyi.com>
2020-08-07 18:51:00 +0200

    Fixing root components

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.scss';
-import { App } from './app/App';
+import { App } from '~app/App';
+
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { store } from '~store/Store';
+
+import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // TODO: enable service worker when everything is ready
