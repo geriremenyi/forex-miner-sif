df86ab3b310fd30bdc5c2077fa994f5bd26b6a2c
Gergely Reményi <geri@geriremenyi.com>
2020-08-18 19:30:00 +0200

    User login store

--- src/app/user/login/store/LoginSlice.ts
+++ src/app/user/login/store/LoginSlice.ts
@@ -1,31 +1,32 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-import { UserConstants, IUser } from '~app/user/shared/types';
-import { ActionName } from '~utilities/types';
+import { UserConstants } from '~app/user/shared/types';
+import { ActionName } from '~common/types';
 
 import { ILoginState } from './ILoginState';
+import { IUser } from '~api/models/user';
 
 const loginInitialState: ILoginState = {
-  isUserLoggedIn: false,
+	isUserLoggedIn: false,
 };
 
 const loginActionNames = {
-  LOGIN: new ActionName('LOGIN'),
-  LOGOUT: new ActionName('LOGOUT'),
-  REGISTER: new ActionName('REGISTER'),
+	LOGIN: new ActionName('LOGIN'),
+	LOGOUT: new ActionName('LOGOUT'),
+	REGISTER: new ActionName('REGISTER'),
 };
 
 export const loginSlice = createSlice({
-  name: UserConstants.SliceName,
-  initialState: loginInitialState,
-  reducers: {
-    [loginActionNames.LOGIN.SUCCESS]: (state: ILoginState, action: PayloadAction<IUser>) => {
-      state.isUserLoggedIn = true;
-      state.loggedInUser = action.payload;
-    },
-    [loginActionNames.LOGOUT.SUCCESS]: (state: ILoginState) => {
-      state.isUserLoggedIn = true;
-      state.loggedInUser = undefined;
-    },
-  },
+	name: UserConstants.SliceName,
+	initialState: loginInitialState,
+	reducers: {
+		[loginActionNames.LOGIN.SUCCESS]: (state: ILoginState, action: PayloadAction<IUser>) => {
+			state.isUserLoggedIn = true;
+			state.loggedInUser = action.payload;
+		},
+		[loginActionNames.LOGOUT.SUCCESS]: (state: ILoginState) => {
+			state.isUserLoggedIn = true;
+			state.loggedInUser = undefined;
+		},
+	},
 });
